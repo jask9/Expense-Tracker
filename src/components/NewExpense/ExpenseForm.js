3f81692cf33923a2edd6a6a8c41391f59bb75f84
jask9 <jaspreetkaur2964@gmail.com>
2023-04-10 01:33:41 +0530

    Update date in Expense Form.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -75,7 +75,7 @@ function ExpenseForm(props) {
             onChange={dateChangeHandler}
             type="date"
             min="2019-01-01"
-            max="2022-12-31"
+            max="2023-12-31"
             value={enteredDate}
           />
         </div>
